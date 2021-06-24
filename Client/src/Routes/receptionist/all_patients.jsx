8266e400a4cd423ea8a14899bcf52534f0711a56
XamuelUG <samweko@gmail.com>
2021-06-24 11:30:26 +0300

    doctor referral

--- Client/src/Routes/receptionist/all_patients.jsx
+++ Client/src/Routes/receptionist/all_patients.jsx
@@ -1,5 +1,21 @@
-import React, { Component } from "react";
-import { Button, TextField } from "@material-ui/core";
+import React, { Component, useState } from "react";
+import {
+  Button,
+  TextField,
+  Menu,
+  MenuItem,
+  FormControl,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Radio,
+  FormControlLabel,
+  FormHelperText,
+  RadioGroup,
+  FormLabel,
+} from "@material-ui/core";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import UsersApi from "../../api/users";
@@ -56,28 +72,7 @@ class AllPatients extends Component {
                           </tr>
                         ) : (
                           this.state.patients.map((v, i) => {
-                            return (
-                              <tr key={i}>
-                                <td>{v.patient_number}</td>
-                                <td>{v.surname}</td>
-                                <td>{v.first_name}</td>
-                                <td>{v.age}</td>
-                                <td>{v.gender}</td>
-                                <td>{v.phone_number}</td>
-                                <td>
-                                  <Button variant="contained" color="primary">
-                                    Edit
-                                  </Button>
-                                </td>
-                                <td>
-                                  <Button variant="contained" color="primary">
-                                    <div className="">
-                                      <i className="las la-print"></i>Print
-                                    </div>
-                                  </Button>
-                                </td>
-                              </tr>
-                            );
+                            return <Row v={v} i={i} key={i} />;
                           })
                         )}
                       </tbody>
@@ -94,3 +89,124 @@ class AllPatients extends Component {
 }
 
 export default AllPatients;
+
+function Row({ v, i }) {
+  const [AnchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [pnumber, setPNumber] = React.useState("");
+
+  const handleOpenActions = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleCloseActions = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClickOpenDialog = (e) => {
+    setPNumber(e.target.dataset.number);
+    setOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <tr key={i}>
+        <td>{v.patient_number}</td>
+        <td>{v.surname}</td>
+        <td>{v.first_name}</td>
+        <td>{v.age}</td>
+        <td>{v.gender}</td>
+        <td>{v.phone_number}</td>
+        <td>
+          <Button variant="contained" color="primary">
+            Edit
+          </Button>
+        </td>
+        <td>
+          <Button
+            variant="contained"
+            color="primary"
+            aria-controls={`reception-actions_${i}`}
+            aria-haspopup="true"
+            onClick={handleOpenActions}
+          >
+            Actions
+          </Button>
+        </td>
+      </tr>
+      <Menu
+        id={`reception-actions_${i}`}
+        anchorEl={AnchorEl}
+        keepMounted
+        disableScrollLock={true}
+        open={Boolean(AnchorEl)}
+        onClose={handleCloseActions}
+      >
+        <MenuItem
+          data-number={v.patient_number}
+          onClick={handleClickOpenDialog}
+        >
+          Assign To Doctor
+        </MenuItem>
+      </Menu>
+      <Dialog
+        open={open}
+        onClose={handleCloseDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        fullWidth
+        maxWidth="xs"
+      >
+        <DialogTitle id="alert-dialog-title">Assign Doctor</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Patient: {pnumber}
+          </DialogContentText>
+          <form>
+            <TextField
+              name="doctor"
+              variant="outlined"
+              label="Doctor"
+              style={{
+                width: "75%",
+                marginBlock: "20px",
+              }}
+            />
+            <FormControl
+              component="fieldset"
+              style={{
+                width: "75%",
+                marginBlock: "20px",
+              }}
+            >
+              <FormLabel component="legend">Reason:</FormLabel>
+              <RadioGroup>
+                <FormControlLabel
+                  value="consultation"
+                  control={<Radio />}
+                  label="Consultation With a Fee"
+                />
+                <FormControlLabel
+                  value="clinical_information"
+                  control={<Radio />}
+                  label="Clinical Information"
+                />
+              </RadioGroup>
+              <FormHelperText>Select Options</FormHelperText>
+            </FormControl>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancel
+          </Button>
+          <Button variant="contained" color="primary" autoFocus>
+            Assign
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "@fontsource/ubuntu";
 
 import App from "./app";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const THEME = createMuiTheme({
+  typography: {
+    fontFamily: `"Ubuntu", sans-serifs`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+});
+const AppWithTheme = () => (
+  <MuiThemeProvider theme={THEME}>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDom.render(<AppWithTheme />, document.getElementById("root"));
