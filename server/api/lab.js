0a1ab6dacac856c289d35f941aaaf903460ac779
MulindwaDenis13 <mulindwadenis13@gmail.com>
2021-07-13 13:51:47 +0300

    change

--- server/api/lab.js
+++ server/api/lab.js
@@ -4,24 +4,25 @@ const conn = require("../database/db");
 router.post("/new_sample_collection", async (req, res) => {
   console.log(req.body);
   let { patient_number, specimens, reason } = req.body;
-  conn.query(
-    `INSERT INTO lab_sample_collection SET ?`,
-    {
-      user_id: 1,
-      patient_id: patient_number,
-      specimens_taken: specimens,
-      reason: reason,
-      collection_date: new Date(),
-    },
-    (err1, res1) => {
-      if (err1) {
-        console.log(err1);
-        res.send({ data: "An Error Occured.Try Again", status: false });
-      } else {
-        res.send({ data: "Sample Collection Added.", status: true });
-      }
-    }
-  );
+
+  // conn.query(
+  //   `INSERT INTO lab_sample_collection SET ?`,
+  //   {
+  //     user_id: 1,
+  //     patient_id: patient_number,
+  //     specimens_taken: specimens,
+  //     reason: reason,
+  //     collection_date: new Date(),
+  //   },
+  //   (err1, res1) => {
+  //     if (err1) {
+  //       console.log(err1);
+  //       res.send({ data: "An Error Occured.Try Again", status: false });
+  //     } else {
+  //       res.send({ data: "Sample Collection Added.", status: true });
+  //     }
+  //   }
+  // );
 });
 
 router.post("/new_lab_report", async (req, res) => {
